9d277a4ada084534fb8d7d82f8928bea1ed08f67
Prachur Gupta <prachur12@gmail.com>
2023-02-01 10:45:31 +0530

    Changed Theme

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,9 @@ import useOnline from "../Utils/useOnline";
 import useAuth from "../Utils/useAuth.js";
 
 const Title = () => (
-  
   <a href="/" className="title-header">
     <img className="h-28 p-2" alt="logo" src={Logo} />
-    <h1>FooD Villa</h1>
+    <h1 className="text-pink-700 font-bold text-4xl font-serif ">FooD Villa</h1>
   </a>
 );
 
@@ -20,8 +19,8 @@ const Header = () => {
   return (
     <div className="flex justify-between bg-pink-50 shadow-lg">
       <Title />
-      <div className="nav-items">
-        <ul className="flex py-10">
+      <div className="nav-items ">
+        <ul className="flex py-10 text-pink-400">
           <li className="px-2">
             <Link to="/"> Home</Link>
           </li>
@@ -37,8 +36,13 @@ const Header = () => {
           <li className="px-2">Cart</li>
         </ul>
       </div>
-      <h1>{isOnline ? "✅" : "🔴"}</h1>
-      <button id="demo">Log In</button>
+      <h1 className="m-10 ">{isOnline ? "" : "🔴"}</h1>
+      <button
+        className="px-4 py-1 m-2 mr-5 bg-pink-400 hover:bg-pink-500 text-white rounded-full border hover:border-pink-600"
+        id="demo"
+      >
+        Log In
+      </button>
       {/* {isLoggedIn ? (
         <button onClick={() => setIsLoggedIn(false)}>LogOut</button>
       ) : (
