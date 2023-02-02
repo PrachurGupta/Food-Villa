c71a8eaa63aff881a6126518fa30f928ce24e904
Prachur Gupta <prachur12@gmail.com>
2023-02-02 12:35:31 +0530

    Bad code for different section of instamart to be displayed one at a time

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,9 +1,83 @@
-const Instamart = () => {
-    return( <div>
-        <h1>Instamart</h1>
-        <h1>100s of components inside it.</h1>
+import { useState } from "react";
+
+const Section = ({ title, description, isVisible, setIsVisible }) => {
+  return (
+    <div className="border border-black p-2 m-2">
+      <h3 className="font-bold text-xl font-serif">{title}</h3>
+      {isVisible ? (
+        <button
+          onClick={() => setIsVisible()}
+          className="cursor-pointer bg-gray-300 rounded-md m-1 px-1 hover:bg-gray-400 hover:text-gray-100"
+        >
+          Hide
+        </button>
+      ) : (
+        <button
+          onClick={() => setIsVisible()}
+          className="cursor-pointer bg-gray-300 rounded-md m-1 px-1 hover:bg-gray-400 hover:text-gray-100"
+        >
+          Show
+        </button>
+      )}
+
+      {isVisible && <p>{description} </p>}
     </div>
-    );
+  );
+};
+
+const Instamart = () => {
+  const [sectionConfig, setSectionConfig] = useState({
+    showAbout: false,
+    showTeam: false,
+    showCareers: false,
+  });
+  return (
+    <div>
+      <h1 className="text-3xl p-2 m-2 font-bold font-serif">Instamart</h1>
+      <Section
+        title={"About Instamart"}
+        description={
+          "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+        }
+        isVisible={sectionConfig.showAbout}
+        setIsVisible={() =>
+          setSectionConfig({
+            showAbout: true,
+            showTeam: false,
+            showCareers: false,
+          })
+        }
+      />
+      <Section
+        title={"Team Instamart"}
+        description={
+          "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+        }
+        isVisible={sectionConfig.showTeam}
+        setIsVisible={() =>
+          setSectionConfig({
+            showAbout: false,
+            showTeam: true,
+            showCareers: false,
+          })
+        }
+      />
+      <Section
+        title={"Careers"}
+        description={
+          "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+        }
+        isVisible={sectionConfig.showCareers}
+        setIsVisible={() =>
+          setSectionConfig({
+            showAbout: false,
+            showTeam: false,
+            showCareers: true,
+          })
+        }
+      />
+    </div>
+  );
 };
 
 export default Instamart;
